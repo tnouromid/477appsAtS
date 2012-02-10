2ff0b20b1603fcc3bd4c21100b6f3498d3f98bf7
Ari Becker <wickedpheonix@gmail.com>
2012-02-10 05:35:28 -0800

    new ruby project

---
+++ AppsAtS/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppsAtS::Application.config.secret_token = '9e917def8384ee4add6554003150c062a5e86887f8a57c78f8a39877d2e47dbe94407f1b3b8f69066dfbe39ef2e90e872405c257d16e284961fa15f1b6ea9966'
